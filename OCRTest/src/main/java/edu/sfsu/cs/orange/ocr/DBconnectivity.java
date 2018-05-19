28e5924bc87a698c22f2a52322fa765278ffa98c
Atews16 <swetasamaddar16@gmail.com>
2018-05-19 12:34:06 +0530

    Initial commit

---
+++ OCRTest/src/main/java/edu/sfsu/cs/orange/ocr/DBconnectivity.java
@@ -0,0 +1,16 @@
+package edu.sfsu.cs.orange.ocr;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class DBconnectivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dbconnectivity);
+
+
+
+    }
+}
